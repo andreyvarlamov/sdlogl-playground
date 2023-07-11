7da67c5434f43859cf4805375011c6527b1fb32e
andreyvarlamov <35208582+andreyvarlamov@users.noreply.github.com>
2023-07-10 18:42:17 -0700

    Creating font atlas (WIP)

--- src/Text.cpp
+++ src/Text.cpp
@@ -9,6 +9,15 @@
 #include <cstdio>
 #include <cstdlib>
 
+struct glyph_info
+{
+    i32 MinX;
+    i32 MaxX;
+    i32 MinY;
+    i32 MaxY;
+    i32 Advance;
+};
+
 u32
 DEBUG_RenderTextIntoTexture(const char *FontPath, const char *Text, glm::mat4 *Out_NativeScaleTransform)
 {
@@ -17,9 +26,94 @@ DEBUG_RenderTextIntoTexture(const char *FontPath, const char *Text, glm::mat4 *O
     TTF_Font *Font = NULL;
     i32 FontSizePoints = 20;
     Font = TTF_OpenFont(FontPath, FontSizePoints);
-    SDL_Color TextColor = { 255, 255, 255, 255 };
+
+    i32 FontHeight = TTF_FontHeight(Font);
+
+    glyph_info GlyphInfos[128];
+
+    i32 MaxGlyphWidth = 0;
+    i32 MaxGlyphHeight = FontHeight;
+    for (u8 Glyph = 32; Glyph < 127; ++Glyph)
+    {
+        i32 *MinX = &(GlyphInfos[Glyph].MinX);
+        i32 *MaxX = &(GlyphInfos[Glyph].MaxX);
+        i32 *MinY = &(GlyphInfos[Glyph].MinY);
+        i32 *MaxY = &(GlyphInfos[Glyph].MaxY);
+        i32 *Advance = &(GlyphInfos[Glyph].Advance);
+        TTF_GlyphMetrics(Font, Glyph, MinX, MaxX, MinY, MaxY, Advance);
+
+        i32 Width = *MaxX - *MinX;
+        if (Width > MaxGlyphWidth)
+        {
+            MaxGlyphWidth = Width;
+        }
+        i32 Height = *MaxY - *MinY;
+        if (Height > MaxGlyphHeight)
+        {
+            MaxGlyphHeight = Height;
+        }
+    }
+
+    // 12x8 = 96 -> for 95 (visible) glyphs
+    i32 AtlasWidth = 12 * MaxGlyphWidth;
+    i32 AtlasHeight = 8 * MaxGlyphHeight;
+    SDL_Surface *FontAtlas = SDL_CreateRGBSurface(0,
+                                                  AtlasWidth,
+                                                  AtlasHeight,
+                                                  32,
+                                                  0x000000FF,
+                                                  0x0000FF00,
+                                                  0x00FF0000,
+                                                  0xFF000000);
+    i32 BytesPerPixel = 4;
+    Assert(FontAtlas->format->BytesPerPixel == BytesPerPixel);
+    Assert((FontAtlas->pitch) == (FontAtlas->w * FontAtlas->format->BytesPerPixel));
+
+    SDL_Color WhiteColor = { 255, 255, 255, 255 };
+
+    i32 CurrentAtlasIndex = 0;
+    for (u8 Glyph = 32; Glyph < 127; ++Glyph)
+    {
+        SDL_Surface *RenderedGlyph = TTF_RenderGlyph_Blended(Font, Glyph, WhiteColor);
+        Assert(RenderedGlyph);
+        Assert(RenderedGlyph->format->BytesPerPixel == BytesPerPixel);
+
+        i32 GlyphWidth = RenderedGlyph->w;
+        i32 GlyphHeight = RenderedGlyph->h;
+        Assert(GlyphWidth <= MaxGlyphWidth);
+        Assert(GlyphHeight <= MaxGlyphHeight);
+
+        i32 CurrentAtlasRow = CurrentAtlasIndex / AtlasWidth;
+        i32 CurrentAtlasCol = CurrentAtlasIndex % AtlasWidth;
+        i32 GlyphRowOffset = CurrentAtlasRow * MaxGlyphHeight;
+        i32 GlyphColOffset = CurrentAtlasCol * MaxGlyphWidth;
+        i32 GlyphOffset = GlyphRowOffset + GlyphColOffset;
+        i32 ByteOffset = GlyphOffset * BytesPerPixel;
+
+        u8 *DestBytes = ((u8 *) FontAtlas->pixels);
+        DestBytes += ByteOffset;
+        u8 *SourceBytes = (u8 *) RenderedGlyph->pixels;
+        for (i32 GlyphRow = 0; GlyphRow < GlyphHeight; + GlyphRow)
+        {
+            for (i32 GlyphCol = 0; GlyphCol < GlyphWidth; ++GlyphCol)
+            {
+                u8 *DestPixelByte = DestBytes + GlyphCol * BytesPerPixel;
+                u8 *SourcePixelByte = SourceBytes + GlyphCol * BytesPerPixel;
+                for (i32 ByteIndex = 0; ByteIndex < BytesPerPixel; ++ByteIndex)
+                {
+                    *DestPixelByte++ = *SourcePixelByte++;
+                }
+            }
+            SourceBytes += RenderedGlyph->pitch * BytesPerPixel;
+            DestBytes -= MaxGlyphWidth * BytesPerPixel;
+        }
+
+        SDL_FreeSurface(RenderedGlyph);
+    }
+    SDL_SaveBMP(FontAtlas, "test.bmp");
+
     SDL_Color BgColor = { 0, 0, 0, 255 };
-    SDL_Surface *RenderedText = TTF_RenderText_Blended(Font, Text, TextColor);
+    SDL_Surface *RenderedText = TTF_RenderText_Blended(Font, Text, WhiteColor);
     Assert(RenderedText);
     Assert(RenderedText->format->BytesPerPixel == 4);
 
@@ -28,21 +122,7 @@ DEBUG_RenderTextIntoTexture(const char *FontPath, const char *Text, glm::mat4 *O
     i32 SourcePitch = RenderedText->pitch;
     i32 DestPitch = Width * RenderedText->format->BytesPerPixel;
     u8 *ProcessedPixels = (u8 *) malloc(Height * DestPitch);
-    u8 *SourcePixels = (u8 *) RenderedText->pixels;
-    u8 *DestPixels = ProcessedPixels + (Height - 1) * DestPitch;
-    for (i32 Row = 0; Row < Height; ++Row)
-    {
-        Assert(DestPixels > 0);
-        Assert((SourcePixels - RenderedText->pixels) < Height*SourcePitch);
-        for (i32 Column = 0; Column < DestPitch; ++Column)
-        {
-            *(DestPixels + Column) = *(SourcePixels + Column);
-        }
-        SourcePixels += SourcePitch;
-        DestPixels -= DestPitch;
-    }
 
-    //SDL_SaveBMP(RenderedText, "test.bmp");
 
     //fprintf(stderr, "%s\n", SDL_GetError());
 
@@ -58,6 +138,7 @@ DEBUG_RenderTextIntoTexture(const char *FontPath, const char *Text, glm::mat4 *O
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
     glBindTexture(GL_TEXTURE_2D, 0);
 
+    TTF_CloseFont(Font);
     SDL_FreeSurface(RenderedText);
     free(ProcessedPixels);
 
